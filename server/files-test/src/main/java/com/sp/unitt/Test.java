4be43a9536b25b6c77dd4c57c1545b2a28c3be92
jongwon <jongwons.choi@gmail.com>
2021-09-10 11:56:00 +0900

    first commit

---
+++ server/files-test/src/main/java/com/sp/unitt/Test.java
@@ -0,0 +1,8 @@
+package com.sp.unitt;
+
+import com.sp.unitt.files.domain.Person;
+
+public class Test {
+
+    Person person;
+}
---
+++ sp/files/src/main/java/com/sp/unitt/files/domain/Person.java
@@ -0,0 +1,18 @@
+package com.sp.unitt.files.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Person {
+
+    private String name;
+
+
+}
